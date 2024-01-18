97dd2ca87d8dd845e01108776ee2240bff06e7ea
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2024-01-18 21:01:48 +0100

    blog post crud

--- example-app/app/Http/Controllers/Api/BlogPostsController.php
+++ example-app/app/Http/Controllers/Api/BlogPostsController.php
@@ -2,48 +2,42 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\BlogPost;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class UsersController extends Controller
+
+class BlogPostsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+   public function index()
+   {
+       return BlogPost::all();
+   }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+   public function store(Request $request)
+   {
+       $blogPost = BlogPost::create($request->all());
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
+       return response()->json($blogPost, 201);
+   }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+   public function show(BlogPost $blogPost)
+   {
+       return response()->json($blogPost);
+   }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+   public function update(Request $request, BlogPost $blogPost)
+   {
+       $blogPost->update($request->all());
+
+       return response()->json($blogPost);
+   }
+
+   public function destroy(BlogPost $blogPost)
+   {
+       $blogPost->delete();
+
+       return response()->json(null, 204);
+   }
+
 }
--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +19,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
 Route::get('posts', [Api\BlogPostsController::class, 'index']);
 Route::post('posts', [Api\BlogPostsController::class, 'store']);
 Route::get('posts/{post}', [Api\BlogPostsController::class, 'show']);
 Route::put('posts/{post}', [Api\BlogPostsController::class, 'update']);
 Route::delete('posts/{post}', [Api\BlogPostsController::class, 'destroy']);
+
